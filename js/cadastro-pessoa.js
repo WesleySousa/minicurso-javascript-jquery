1f017b15b95a817ad474d103cc1ebda2ea60dc97
Wesley Sousa Silva <wesley.sousa59@gmail.com>
2018-01-19 09:57:05 -0300

    feito adição dos dados do formulario dinamicamente na tabela.

--- js/cadastro-pessoa.js
+++ js/cadastro-pessoa.js
@@ -1,13 +1,41 @@
 $(function(){
+	var incr = 1;
+	var pessoas = [{}];
 	$('#pessoa').submit(function(event){
 		event.preventDefault();
 		let dados = $(this).serializeArray();	
-		console.log(dados);
+		
+		let idade = 21;
+		let pessoa = {id: incr, nome: dados[0].value, email: dados[1].value};
+		pessoas.push(pessoa);
+		incr++;
 
-		let pessoa = {nome: '', email: ''};
-		let pessoas;
+		$('table tbody').append(
+				$('<tr/>')
+					.append(
+						$('<td/>').append(pessoa.id)
+					)
+					.append(
+						$('<td/>').append(pessoa.nome)
+					)
+					.append(
+						$('<td/>').append(pessoa.email)
+					)
+					.append(
+						$('<td/>').append(
+								$('<button/>').addClass('rm').text('Remover')
 
+							)
+					)
+			);
 
-		console.log(pessoas);
+		$('[name=nome]').val('');
+		$('[name=email]').val('');
+		$('[name=nome]').focus();
 	});
+
+	$(document).on('click', '.rm', function(event){
+		$(this).parent().parent().remove();
+	});
+
 });
